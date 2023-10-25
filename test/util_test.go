82a64d247001a850288de2d1f8be1fa15e491de0
zing <v_zxxzyao@tencent.com>
2023-10-25 11:25:43 +0800

    feat: 更新单元测试 允许保存进度

--- test/util_test.go
+++ test/util_test.go
@@ -3,10 +3,13 @@ package test
 import (
 	"fmt"
 	"github.com/ZingYao/chinese_number"
+	"io"
 	"math"
 	"os"
+	"strconv"
 	"sync"
 	"testing"
+	"time"
 )
 
 func TestStr2Num(t *testing.T) {
@@ -85,10 +88,33 @@ func TestNumber2Chinese(t *testing.T) {
 }
 
 func TestAllNum(t *testing.T) {
+	currentNum := int64(math.MinInt64)
+	file, err := os.OpenFile("test_all_num.save", os.O_CREATE|os.O_WRONLY|os.O_RDWR, 0644)
+	if err != nil {
+		t.Errorf("open save file failed,%+v", err)
+		return
+	}
+	defer file.Close()
+	defer func() {
+		// 程序退出前 记录当前的循环数字
+		_, err = file.WriteString(strconv.FormatInt(currentNum, 10))
+		if err != nil {
+			t.Errorf("save process failed,%+v", err)
+		}
+	}()
+	const thread = 64
+	processBytes, err := io.ReadAll(file)
+	process := currentNum
+	if err == nil {
+		n, err := strconv.ParseInt(string(processBytes), 10, 64)
+		if err == nil {
+			process = n
+		}
+	}
 	index := uint64(0)
-	limit := make(chan int, 64)
+	limit := make(chan int, thread)
 	wg := sync.WaitGroup{}
-	for i := int64(math.MinInt64); i < math.MaxInt64; i++ {
+	for i := process; i < math.MaxInt64; i++ {
 		index++
 		wg.Add(1)
 		limit <- 1
@@ -106,9 +132,12 @@ func TestAllNum(t *testing.T) {
 				fmt.Println("err: ", str, number, num)
 				os.Exit(-1)
 			}
+			if number > currentNum {
+				currentNum = number
+			}
 		}(i)
 		if index%10000000 == 0 {
-			fmt.Printf("%.010f%%\n", float64(index)/math.MaxInt64*100*2)
+			fmt.Printf("[%s]%.010f%%\n", time.Now().Format("2006-01-02 15:04:05"), float64(index)/math.MaxInt64*100*2)
 		}
 	}
 	wg.Wait()
